1d42f3fa55bb5de9272ba1347491d7a5d127d864
balex0319 <borissskk@tutamail.com>
2025-12-04 18:51:52 +0300

    perfection.py

--- perfection.py
+++ perfection.py
@@ -1,23 +1,18 @@
-def summa(a, b):
-    if b == sum(a):
-        print('Число совершенное')
-    else:
-        print('Число несовершенное')
-
-def div(n):
-    a = []
-    for i in range(1, n):
-        if n % i == 0:
-            a.append(i)
-    return a
-b = int(input('Введите число: '))
-print(div(b))
-summa(div(b), b)
+def perf():
+    a = int(input('Введите число: '))
+    b = 0
+    for i in range(1, a):   
+        if a % i == 0:
+            b += i
+    print('Сумма делителей: ', b)
+    if a == b: print('Число совершенное')
+    else: print('Число не совершенное')
+    return perf()
+if __name__== '__main__':
+    perf()
 
 def main():
-    b = int(input('Введите число: '))
-    print(div(b))
-    summa(div(b), b)
-    
+    a = int(input())
+    b = 0
 if __name__ == "__main__":  
     main()
