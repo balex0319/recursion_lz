5b16d0aad8de8e6ed1e384d06578636c4f72a397
balex0319 <borissskk@tutamail.com>
2025-12-04 22:37:46 +0300

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,20 @@
+a = (input('Введите первое число:'))
+b = (input('Введите второе число:'))
+def addition(a, b, c=0):
+    if not a and not b and c == 0:
+        return ''
+    d1 = int(a[-1]) if a else 0
+    d2 = int(b[-1]) if b else 0
+    summa = d1 + d2 + c
+    c = summa // 10
+    x = summa % 10
+    return addition(a[:-1], b[:-1], c) + str(x)
+print('Сумма чисел равна: ', addition(a, b))
+
+def main(): 
+    a = input()
+    b = input
+    d1 = int()
+    d2 = int()
+if __name__ == "__main__":  
+    main()
---
+++ launch.py
@@ -0,0 +1,12 @@
+import Euclid
+import Pascal
+import perfection
+import addition
+
+def main():
+      Euclid.main()
+      Pascal.main() 
+      perfection.main()
+      addition.main()
+if __name__ == "__main__":
+        main()
