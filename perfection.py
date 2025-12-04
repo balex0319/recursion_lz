6880356eb6790b9420381053ff642719b1fcd08c
balex0319 <borissskk@tutamail.com>
2025-12-04 18:51:13 +0300

    Add files via upload

---
+++ perfection.py
@@ -0,0 +1,23 @@
+def summa(a, b):
+    if b == sum(a):
+        print('Число совершенное')
+    else:
+        print('Число несовершенное')
+
+def div(n):
+    a = []
+    for i in range(1, n):
+        if n % i == 0:
+            a.append(i)
+    return a
+b = int(input('Введите число: '))
+print(div(b))
+summa(div(b), b)
+
+def main():
+    b = int(input('Введите число: '))
+    print(div(b))
+    summa(div(b), b)
+    
+if __name__ == "__main__":  
+    main()
