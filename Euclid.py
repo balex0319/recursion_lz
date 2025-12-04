80e54c70a66d2b677e7c3add9f6a5b74147e94a4
balex0319 <borissskk@tutamail.com>
2025-12-04 15:57:52 +0300

    Euclid.py

--- Euclid.py
+++ Euclid.py
@@ -1,7 +1,8 @@
-def Evclid(a1, a2):
+def Euclid(a1, a2):
     if a1 == 0:
         return a2
-    return Evclid(a2 % a1, a1)
+    return Euclid(a2 % a1, a1)
 number_1 = input('Введите первое число: ')
 number_2 = input('Введите второе число: ')
-print(f'НОД: {Evclid (int(number_1), int(number_2))}')
+
+print(f'НОД: {Euclid (int(number_1), int(number_2))}')
